9859501d1ed62c74041a3b316dbea5b68a016e07
ckxgzxa <ckxgzxa@qq.com>
2022-04-05 23:57:56 +0800

    final

--- src/main/scala/CSV.scala
+++ src/main/scala/CSV.scala
@@ -4,12 +4,13 @@ object CSV {
     def main(args: Array[String]): Unit = {
         val conf = new SparkConf().setAppName("CSV").setMaster("local")
         val sc = new SparkContext(conf)
-        val rdd = sc.textFile("file:///home/hadoop/spark/src/main/resources/data/in/1.csv")
-        val rdd1 = rdd.map(line => {
-            val arr = line.split(",")
-            (arr(0).toInt+arr(1).toInt+arr(2).toInt)
-        })
-        //将结果保存到文件中
-        rdd1.saveAsTextFile("file:///home/hadoop/spark/src/main/resources/data/out/")
+        // 从HDFS中读取数据
+        val rdd = sc.textFile("/user/hadoop/data/input/5.csv")
+        val rdd1 = rdd.map(x => {
+              val arr = x.split(",")
+              (arr(0), arr(1))
+        }) 
+        //将结果保存到本地文件系统中
+        rdd1.saveAsTextFile("file:///home/hadoop/spark/src/main/resources/data/out/vsv")
     }
 }
