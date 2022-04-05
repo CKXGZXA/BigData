5dc18c31b06b3a89ab0c62dba54449361aae8917
ckxgzxa <ckxgzxa@qq.com>
2022-04-05 23:49:23 +0800

    更新wd

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -4,10 +4,10 @@ object WordCount {
    def main(args: Array[String]): Unit = {
         val conf = new SparkConf().setAppName("WordCount").setMaster("local")
         val sc = new SparkContext(conf)
-        val lines = sc.textFile("file:///home/hadoop/spark/src/main/resources/data/in/word.txt")
+        val lines = sc.textFile("/user/hadoop/data/input/6.txt")
         val words = lines.flatMap(line => line.split(" "))
         val pairs = words.map(word => (word, 1))
         val wordCounts = pairs.reduceByKey(_ + _)
-        wordCounts.foreach(wordCount => println(wordCount._1 + " : " + wordCount._2))
+        wordCounts.saveAsTextFile("file:///home/hadoop/spark/src/main/resources/data/out/wordcount")
    }
 }
